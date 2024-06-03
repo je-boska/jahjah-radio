77e86f9ba0117c8f31cfdcfddb14917f04c6fc5a
je-boska <joneirikboska@gmail.com>
2024-06-03 11:15:13 +0200

    Add types

---
+++ types/shared.d.ts
@@ -0,0 +1,49 @@
+export interface EventInterface {
+  id: string;
+  stationId: string;
+  title: string;
+  startDateUtc: string;
+  endDateUtc: string;
+  description?: JSONContent;
+  duration: Minutes;
+  timezone: string;
+  color?: string;
+  artistIds?: string[];
+  isRecurring: boolean;
+  media:
+    | {
+        type: "mix";
+        trackId?: string | undefined;
+      }
+    | {
+        type: "playlist";
+        playlistId: string;
+      }
+    | {
+        type: "live";
+      };
+}
+
+export interface SuccessfulLiveAPIResponse {
+  success: true;
+  result:
+    | { status: "schedule"; content: EventInterface }
+    | { status: "offAir"; content: "Off Air" }
+    | {
+        status: "defaultPlaylist";
+        content: {
+          name: string;
+          numberOfSongs: number;
+          duration: Minutes;
+        };
+      };
+}
+
+export interface UnsuccessfulLiveAPIResponse {
+  success: false;
+  error?: string;
+}
+
+export type LiveAPIResponse =
+  | SuccessfulLiveAPIResponse
+  | UnsuccessfulLiveAPIResponse;
