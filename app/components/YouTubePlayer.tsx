0c1e7ac5e28eabb6e4682fcc93d85482db0f5558
je-boska <joneirikboska@gmail.com>
2024-06-13 15:26:08 +0200

    Replace youtube link

--- app/components/YouTubePlayer.tsx
+++ app/components/YouTubePlayer.tsx
@@ -1,21 +1,9 @@
-"use client";
-
-import { shouldUnloadLivePlayerState } from "../hooks/usePlayerState";
-
 export default function YouTubePlayer() {
-  const [shouldUnloadLivePlayer, setShouldUnloadLivePlayer] =
-    shouldUnloadLivePlayerState.use();
-
   return (
     <div className="w-full px-4 md:px-20 xl:px-40">
       <iframe
-        onClick={() => {
-          if (!shouldUnloadLivePlayer) {
-            setShouldUnloadLivePlayer(true);
-          }
-        }}
         className="aspect-video h-full w-full"
-        src="https://www.youtube.com/embed/fkh9PnwA9DA?si=wx1UNod7FJEEocYh"
+        src="https://www.youtube.com/embed/6y60GgNqFg0?si=wJSZ2VVhSdA7fM8k"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
